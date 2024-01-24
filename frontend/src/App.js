0b8fa825f4a7540ae09abe4066ea45989dc357d3
JasserGa <jessergafsi2@gmail.com>
2024-01-24 16:35:48 +0300

    css cards-card_details-add video home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,13 +10,19 @@ function App() {
 
   return (
     <div className="App">
-      <nav>
+      <nav className='navbar'>
       
         <h2 onClick={() => changeView('Films')}>Home</h2>
         <h2 onClick={() => changeView('AddFilm')}>Add Film</h2>
         <h2 onClick={() => changeView('UpdateFilm')}>Update Film</h2>
       </nav>
       <div>
+    <video autoPlay muted loop id="background-video">
+      <source src="video.mp4" type="video/mp4" />
+    </video>
+  </div>
+      <div>
+
         {view === 'Films' && <Films />}
         {/* {view === 'AddFilm' && <AddFilm changeView={changeView} />}
         {view === 'UpdateFilm' && <UpdateFilm changeView={changeView} />} */}
