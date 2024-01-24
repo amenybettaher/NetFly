4a4b767fbd141ea4a568be42c76360ffbab5e910
amenybettaher <bettaherameni1919@gmail.com>
2024-01-24 14:20:43 +0100

    fetching data done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,31 @@
-
+import React, { useState } from 'react';
+import Films from './components/Films.jsx';
 
 function App() {
+  const [view, setView] = useState('Films');
+
+  const changeView = (newView) => {
+    setView(newView);
+  };
+
   return (
     <div className="App">
-     <h1>hello</h1>
+      <nav>
+      
+        <h2 onClick={() => changeView('Films')}>Home</h2>
+        <h2 onClick={() => changeView('AddFilm')}>Add Film</h2>
+        <h2 onClick={() => changeView('UpdateFilm')}>Update Film</h2>
+      </nav>
+      <div>
+        {view === 'Films' && <Films />}
+        {/* {view === 'AddFilm' && <AddFilm changeView={changeView} />}
+        {view === 'UpdateFilm' && <UpdateFilm changeView={changeView} />} */}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+
